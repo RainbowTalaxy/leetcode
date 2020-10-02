be57bb209cb17ce2a08e7a11fc40bd86b71a8501
talaxy <fujianchensiwei@163.com>
2020-10-03 00:55:56 +0800

    提交 1 题

---
+++ 0001. 两数之和/排序.swift
@@ -0,0 +1,18 @@
+// 48ms
+class Solution {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        var sorted = nums.enumerated().map { i in (i.1, i.0)} .sorted { (a, b) in a.0 < b.0 }
+        var left = 0, right = nums.count - 1
+        while (true) {
+            let result = sorted[left].0 + sorted[right].0
+            if result == target {
+                print(result)
+                return [sorted[left].1, sorted[right].1]
+            } else if result > target {
+                right -= 1
+            } else {
+                left += 1
+            }
+        }
+    }
+}

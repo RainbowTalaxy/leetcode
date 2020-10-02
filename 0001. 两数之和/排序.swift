1046fb539530ac3cc1925349a43cd7ecd597d7be
talaxy <fujianchensiwei@163.com>
2020-10-03 00:57:15 +0800

    修改 1 题

--- 0001. 两数之和/排序.swift
+++ 0001. 两数之和/排序.swift
@@ -3,7 +3,7 @@ class Solution {
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
         var sorted = nums.enumerated().map { i in (i.1, i.0) } .sorted { (a, b) in a.0 < b.0 }
         var left = 0, right = nums.count - 1
-        while (true) {
+        while true {
             let result = sorted[left].0 + sorted[right].0
             if result == target {
                 print(result)
